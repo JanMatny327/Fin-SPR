67b379b041b77df65f62dd348c21aac4d439a9a3
김종훈 <133855697+JanMatny327@users.noreply.github.com>
2024-10-05 03:55:33 +0900

    2024-10-05

---
+++ Assets/Script/BossSystem.cs
@@ -0,0 +1,362 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+using static UnityEditor.PlayerSettings;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
+
+public class BossSystem : MonoBehaviour
+{
+    public static BossSystem Instance;
+    // 기본 속성
+    [SerializeField] private int bossHp; // 보스 HP
+    private int maxBossHp; // 최대 보스 Hp
+    private Rigidbody2D rig2D; // 보스 물리
+    private Animator bossAnim; // 보스 애니메이션
+    private BoxCollider2D boxCollider;
+    [SerializeField] private int bossLevel = 1; // 보스 레벨
+    [SerializeField] private Transform target;
+    public int speedX; // rush 최대속도
+    int DIRECTION;
+    bool bossCritical = false;
+
+  
+
+    [Header("BossProfile")]
+    public GameObject bossProfile; // 보스 체력바
+    public Slider hpBar; //HP바
+    public TextMeshProUGUI hpText; // HP바 잔량 표시
+    public Vector2 curPos = new Vector2(49.221f, 14.169f); // 보스의 원래 위치 저장
+
+
+
+    [Header("Boss Bullet List")]
+    [SerializeField] private GameObject[] bossBullet; // 일반 총알 Prefab 배열
+    [SerializeField] private GameObject[] razerBullet; // 레이저 위치
+
+    [Header("Boss Bullet Renderer")]
+    [SerializeField] private SpriteRenderer[] bulletPos;
+
+    [Header("Boss Attack")]
+    [SerializeField] Transform[] spinbulletPos; // 회전 보스 총알 날라갈 위치
+    [SerializeField] Transform[] missilebulletPos; // 유도탄 보스 총알 날라갈 위치
+    [SerializeField] Transform[] turretPos; // 터렛 소환될 위치
+    [SerializeField] Transform[] skyBulletPos; // 하늘 총알 위치
+    [SerializeField] Transform[] splashBulletPos; // 스플레쉬
+    public float attackCoolTime; // 공격 쿨타임
+    private float attackCurTime; // 현재 공격 타임
+    bool isAttack;
+    public int bossDamage;
+
+    [Header("BossPattern")]
+    [SerializeField] private int patternLevel; // 패턴 레벨
+    public float patternCoolTime; // 패턴 쿨타임
+    public float patternWaitTime;
+    public float roopPattern;
+    public bool isPattern;
+    bool isWall = false; // 돌진패턴에 쓰는 것
+
+    [Header("PatternNote")]
+    private static readonly int NONE = 0;
+    private static readonly int SPINBULLET = 1;
+    private static readonly int MISSILE = 2;
+    private static readonly int RUSH = 3;
+
+    [Header("Boss Damage Setting")]
+    public int RushDamage = 45;
+
+    private WaitForSeconds endOfPatternWait;
+    private WaitForSeconds waitOfPatternWait;
+    
+
+    void Start()
+    {
+        Instance = this;
+        rig2D = GetComponent<Rigidbody2D>();
+        bossAnim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        endOfPatternWait = new WaitForSeconds(patternCoolTime); // 패턴 대기시간 : 다음 패턴을 실행할때의 대기시간
+        waitOfPatternWait = new WaitForSeconds(patternWaitTime); // 공격 대기시간 : 자세를 취하고 공격하는 대기시간
+        maxBossHp = bossHp;
+        StartCoroutine(RotateBullet());
+        hpBar.value = (float)bossHp / maxBossHp;
+        lookPlayer();
+    }
+    void Update()
+    {
+        isBossRoom();
+        BossLevel();
+        HandleHp();
+    }
+    void isBossRoom()
+    {
+        if (PlayerController.Instance.isBossRoom == true)
+        {
+            bossProfile.SetActive(true);
+        }
+    }
+    void HandleHp()
+    {
+        hpBar.value = Mathf.Lerp(hpBar.value, (float)bossHp / maxBossHp, Time.deltaTime * 10);
+        hpText.text = bossHp + "/" + maxBossHp;
+    }
+    public void getDamage(int damage)
+    {
+        this.bossHp -= damage;
+        return;
+    }
+    void BossBullet()
+    {
+        if (attackCurTime <= 0)
+        {
+            switch (patternLevel)
+            {
+                case 1:
+                    for (int i = 0; i < spinbulletPos.Length; i++)
+                    {
+                        Instantiate(bossBullet[0], spinbulletPos[i].position, spinbulletPos[i].rotation);
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < missilebulletPos.Length; i++)
+                    {
+                        Instantiate(bossBullet[1], missilebulletPos[i].position, missilebulletPos[i].rotation);
+                    }
+                    break;
+            }
+            attackCurTime = attackCoolTime;
+        }
+        attackCurTime -= Time.deltaTime;
+    }
+    void Idle()
+    {
+        bossAnim.SetInteger("PatternLevel", 0);
+    }
+    void BossLevel()
+    {
+        if (bossHp <= maxBossHp / 2 && bossHp > maxBossHp / 3 && bossCritical == false)
+        {
+            bossLevel = 2;
+            bossAnim.SetBool("isCritical", true);
+            bossCritical = true;
+        }
+
+        /*
+        else if (bossHp <= maxBossHp / 3)
+        {
+            bossLevel = 3;
+            bossAnim.SetBool("isCritical", true);
+            Invoke("BossPattern", 2);
+        }
+        */
+
+    }
+    void lookPlayer()
+    {
+        DIRECTION = (target.position.x < transform.position.x ? -1 : 1); //player와 자신(보스)의 x좌표를 비교해서 적당한 상수를 DIRECTION에 저장한다.
+        float scale = transform.localScale.z;
+        transform.localScale = new Vector3(DIRECTION * -1 * scale, scale, scale); //DIRECTION변수를 이용해서 player쪽을 바라보도록한다.
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerController.Instance.gameData.hp -= this.RushDamage;
+            transform.position = curPos;
+        }
+        else if (collision.gameObject.tag == "Wall")
+        {
+            isWall = true;
+        }
+    }
+
+    IEnumerator roopOutPattern()
+    {
+        yield return new WaitForSeconds(roopPattern);
+        isPattern = false;
+        isWall = true;
+    }
+
+    void BossPattern()
+    {
+        if (bossLevel == 1)
+        {
+            patternLevel = Random.Range(1, 4);
+            switch (patternLevel)
+            {
+                case 1:
+                    StartCoroutine(RotateBullet());
+                    break;
+                case 2:
+                    StartCoroutine(MissileBullet());
+                    break;
+                case 3:
+                    StartCoroutine(Rush());
+                    break;
+            }
+        }
+        else if (bossLevel == 2)
+        {
+            bossAnim.SetBool("isCritical", false);
+            StopCoroutine(RotateBullet());
+            StopCoroutine(MissileBullet());
+            StopCoroutine(Rush());
+            patternLevel = Random.Range(1, 4);
+            switch (patternLevel)
+            {
+                case 1:
+                    StartCoroutine(Turret());
+                    break;
+                case 2:
+                    StartCoroutine(SkyBullet());
+                    break;
+                case 3:
+                    StartCoroutine(Splash());
+                    break;
+            }
+        }
+        else if (bossLevel == 3)
+        {
+
+        }
+
+
+    }
+
+    IEnumerator RotateBullet() // 총알이 회전하면서 날라가는 패턴
+    {
+        patternLevel = 1;
+        bossAnim.SetInteger("PatternLevel", 1);
+        StartCoroutine(roopOutPattern());
+        isPattern = true;
+        while (isPattern)
+        {
+            yield return new WaitForSeconds(0.01f);
+            foreach (Transform bullet1 in spinbulletPos)
+            {
+                bullet1.eulerAngles = new Vector3(0f, 0f, bullet1.eulerAngles.z + 1f);
+                BossBullet();
+            }
+        }
+        Idle();
+        yield return endOfPatternWait;
+        BossPattern();
+    }
+    IEnumerator MissileBullet()
+    {
+        bossAnim.SetInteger("PatternLevel", 2);
+        StartCoroutine(roopOutPattern());
+        isPattern = true;
+        while (isPattern)
+        {
+            yield return new WaitForSeconds(0.01f);
+            foreach (Transform bullet in missilebulletPos)
+            {
+                Vector3 direction = (target.position - bullet.position).normalized;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
+                BossBullet();
+            }
+        }
+        Idle();
+        yield return endOfPatternWait;
+        BossPattern();
+    }
+    IEnumerator Rush()
+    {
+        bossAnim.SetInteger("PatternLevel", 3);
+        lookPlayer();
+        yield return new WaitForSeconds(1f);
+        if (speedX > rig2D.velocity.x)
+        {
+            rig2D.AddForce(transform.right * DIRECTION * 1000);
+        }  
+        Idle();
+        transform.position = curPos;
+        yield return endOfPatternWait;
+        lookPlayer();
+        BossPattern();
+    }
+    IEnumerator Turret()
+    {
+        bossAnim.SetInteger("PatternLevel", 3);
+        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        foreach(SpriteRenderer renderer in bulletPos)
+        {
+            renderer.enabled = true;
+        }
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(roopOutPattern());
+        isPattern = true;
+        while (isPattern)
+        {
+            foreach(Transform Turret in turretPos)
+            {
+                Instantiate(bossBullet[1], Turret.position, Turret.rotation);
+            }
+            yield return new WaitForSeconds(0.03f);
+        }
+        Idle();
+        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        foreach (SpriteRenderer renderer in bulletPos)
+        {
+            renderer.enabled = false;
+        }
+        yield return endOfPatternWait;
+        BossPattern();
+    }
+    IEnumerator SkyBullet()
+    {
+        bossAnim.SetInteger("PatternLevel", 3);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(roopOutPattern());
+        isPattern = true;
+        foreach (GameObject razer in razerBullet)
+        {
+            razer.SetActive(true);
+        }
+        while (isPattern == true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            foreach (Transform bullet in skyBulletPos)
+            {
+                Vector3 direction = (target.position - bullet.position).normalized;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
+                Instantiate(bossBullet[0], bullet.position, bullet.rotation);
+                yield return new WaitForSeconds(0.15f);
+            }
+        }
+        foreach (GameObject razer in razerBullet)
+        {
+            razer.SetActive(false);
+        }
+        Idle();
+        yield return endOfPatternWait;
+        BossPattern();
+    }
+    IEnumerator Splash()
+    {
+        bossAnim.SetInteger("PatternLevel", 3);
+        gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        yield return new WaitForSeconds(0.7f);
+        StartCoroutine(roopOutPattern());
+        isPattern = true;
+        while (isPattern)
+        {
+            yield return new WaitForSeconds(1f);
+            foreach (Transform bullet in splashBulletPos)
+            {
+                Instantiate(bossBullet[0], bullet.position, bullet.rotation);
+            }
+        }
+        Idle();
+        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        yield return endOfPatternWait;
+        BossPattern();
+
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+[System.Serializable]
+public class GameData
+{
+    [Header("성장 관리")]
+    public int level = 1; // 플레이어 레벨
+    public int[] levelUpExpBox = { 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
+                                   105, 115, 125, 135, 145, 155, 165, 175, 195, 205,
+                                   220, 235, 250, 265, 280, 295, 310, 325, 340, 355,
+                                   375, 395, 415, 435, 455, 475, 495, 515, 535, 555,
+                                   580, 605, 630, 655, 680, 705, 730, 755, 780, 805 }; // 플레이어가 레벨업을 하기 위한 경험치 필요량을 담은 배열
+    public int exp = 0; // 플레이어 경험치
+    public int statPoint = 0; // 스텟 포인트
+    public int maxLevelValue = 50;
+
+    [Header("Stats")]
+    public int gold; // 플레이어 골드
+    public int maxHP = 100; // 플레이어 최대 체력
+    public int hp; // 플레이어 현재 체력
+    public int damage; // 플레이어 데미지
+    public float criticalPercent; // 크리티컬 확률
+    public float criticalDamage; // 크리티컬 데미지
+    public float playerSpeed; // 플레이어 이동속도
+    public float jumpPower; // 점프력
+    public bool isJump = false;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameData gameData;
+    public GameObject Panel;
+    public GameObject player;
+
+    [Header("공지 텍스트")]
+    public TMP_Text gameBroadText;
+    private bool isMessage = false;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        SettingUI();
+        StartCoroutine(BroadCastManager());
+    }
+
+    [ContextMenu("Save Data")]
+    void SaveData()
+    {
+        string data = JsonUtility.ToJson(gameData);
+        string path = Path.Combine(Application.dataPath, "GameData.json");
+        File.WriteAllText(path, data);
+    }
+
+    [ContextMenu("Load Data")]
+    void LoadData()
+    {
+        SceneManager.LoadScene("GameScenes");
+
+        string path = Path.Combine(Application.dataPath, "GameData.json");
+        if (!File.Exists(path))
+        {
+            SaveData();
+        }
+        string data = File.ReadAllText(path);
+
+        gameData = JsonUtility.FromJson<GameData>(data);
+    }
+
+    public void SettingUI()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !PlayerController.Instance.dieState && !PlayerController.Instance.isCutScene)
+        {
+            if (!Panel.activeSelf)
+            {
+                Panel.SetActive(true);
+                player.SetActive(false);
+            }
+            else
+            {
+                Panel.SetActive(false);
+                player.SetActive(true);
+            }
+        }
+    }
+
+    public void LevelUpBroad()
+    {
+        gameBroadText.text = PlayerController.Instance.gameData.level + "레벨이 되었습니다!";
+        gameBroadText.color = Color.green;
+        isMessage = true;
+    }
+
+    private IEnumerator BroadCastManager()
+    {
+        while (isMessage)
+        {
+            yield return new WaitForSeconds(0.55f);
+            gameBroadText.text = "";
+            isMessage = false;
+        }
+    }
+}
+
